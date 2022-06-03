8a610f0bf4db438b59a1dca6e3233e6a09f93d4c
joseorozco84 <joseorozco84@gmail.com>
2022-06-03 01:31:13 -0300

    contacts changes

--- contacts/views.py
+++ contacts/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def contacts(request):
     return render(request, 'contacts/contacts.html')
+
+
+def contact_success(request):
+    return render(request, 'contacts/contact_success.html')
