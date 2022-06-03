c82a7d7049f83a49871e94ed6020615a1351bb74
claracena <claracena@users.noreply.github.com>
2022-06-02 23:19:15 -0300

    Initial config app and changes to error files

--- config/views.py
+++ config/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.core.exceptions import PermissionDenied
 
 # Create your views here.
+def config(request):
+    if request.user.is_superuser:
+        return render(request, 'config/index.html')
+    else:
+        raise PermissionDenied()
+    
