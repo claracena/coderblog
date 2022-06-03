eb5ea810b09a9f823f5ddb324035402c87fe91fa
Esteban1191 <esteban.sancor@gmail.com>
2022-06-03 12:18:46 -0300

    avatars

--- about/models.py
+++ about/models.py
@@ -5,6 +5,6 @@ from django.db import models
 
 class About(models.Model):
     name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    info = models.TextField()
+    info = models.TextField(max_length=300)
     updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(null=True, default='avatar.jpg')
