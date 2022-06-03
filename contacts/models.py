92071384e33934fc4d3a000411404483cfeabd55
joseorozco84 <joseorozco84@gmail.com>
2022-06-03 12:56:23 -0300

    contacts admin page changes

--- contacts/models.py
+++ contacts/models.py
@@ -6,6 +6,7 @@ class Contacts(models.Model):
     name = models.CharField(max_length=50, null=False)
     email = models.EmailField(null=False)
     message = models.TextField(null=False)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.email
+        return self.email + ' - ' + self.created.strftime('%d/%m/%Y %H:%M')
