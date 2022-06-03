0a2a7843da1cb6feb856d6b5cfaeb55f0407b9f3
joseorozco84 <joseorozco84@gmail.com>
2022-06-02 21:34:13 -0300

    contacts model update

--- contacts/models.py
+++ contacts/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Contacts(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    message = models.TextField()
+    name = models.CharField(max_length=50, null=False)
+    email = models.EmailField(null=False)
+    message = models.TextField(null=False)
