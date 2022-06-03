842594646458fe9e76aaef6da038a8649c53b7b5
joseorozco84 <joseorozco84@gmail.com>
2022-06-02 21:08:15 -0300

    contacts

--- coderblog/settings.py
+++ coderblog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'login.apps.LoginConfig',
     'blog.apps.BlogConfig',
+    'contacts.apps.ContactsConfig',
 ]
 
 AUTH_USER_MODEL = 'login.User'
