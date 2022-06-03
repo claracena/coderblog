044749c205835ba4b2b12ef6fbe2476f898a116c
Esteban1191 <esteban.sancor@gmail.com>
2022-06-03 00:56:28 -0300

    model creation

--- about/models.py
+++ about/models.py
@@ -1,3 +1,11 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class About(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    info = models.TextField()
+    updated = models.DateTimeField('auto_now=True')
+    image = models.ImageField()
