9e904475ba08ca7355eb70eb16fe9ed2bd6801fe
Esteban1191 <esteban.sancor@gmail.com>
2022-06-04 01:07:02 -0300

    changes + bday

--- about/models.py
+++ about/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 class About(models.Model):
     name = models.CharField(max_length=30)
+    bday = models.DateField(null=True)
     info = models.TextField(max_length=300)
     updated = models.DateTimeField(auto_now=True)
     image = models.ImageField(null=True, default='avatar.jpg')
+    def __str__(self):
+        return self.name
