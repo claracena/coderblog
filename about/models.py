4a19ea10bc8085f55381e9bcfbaae69abb8bb364
claracena <claracena@users.noreply.github.com>
2022-06-05 19:31:33 -0300

    Fixes after final merge

--- about/models.py
+++ about/models.py
@@ -2,12 +2,3 @@ from unicodedata import name
 from django.db import models
 
 # Create your models here.
-
-class About(models.Model):
-    name = models.CharField(max_length=30)
-    bday = models.DateField(null=True)
-    info = models.TextField(max_length=300)
-    updated = models.DateTimeField(auto_now=True)
-    image = models.ImageField(null=True, default='avatar.jpg')
-    def __str__(self):
-        return self.name
