60ffec59f43e3a64495c1500105eafff65a1cdec
claracena <claracena@users.noreply.github.com>
2022-06-05 13:43:29 -0300

    Search button working

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -5,3 +5,15 @@
 /*===================================================================================*/
 
 // Insert your own scripts in here!
+$('li.searchbox a').on('click', function (event) {
+    $(this).parent().toggleClass('open');
+});
+
+$('body').on('click', function (e) {
+    if (!$('li.searchbox').is(e.target) 
+        && $('li.searchbox').has(e.target).length === 0 
+        && $('.open').has(e.target).length === 0
+    ) {
+        $('li.searchbox').removeClass('open');
+    }
+});
